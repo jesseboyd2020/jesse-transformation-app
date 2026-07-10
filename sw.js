8b79c93130379eba2c119748597f2ba22b540ae2
jesseboyd2020 <jesse.boyd2020@gmail.com>
2026-07-10 17:30:14 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='jesse-elite-v3';const A=['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
